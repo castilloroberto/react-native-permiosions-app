c9ef27ba2dce5d27fe03a27599e5270e7918b2be
robertocastillo <robertocastillo945@gmail.com>
2021-08-21 14:37:50 -0600

    finañ

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { Pressable,PermissionsAndroid, StyleSheet, Text, View, Button } from 'react-native';
+
+const askForPhoneCallPermission = async ()=>{
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CALL_PHONE,
+      {
+        title:"Permiso de acceso",
+        message:"Esta app necesirta acceder a funciones de llamada telefonica",
+        buttonNeutral:"Preguntar luego",
+        buttonNegative:"Cancelar",
+        buttonPositive:"OK"
+      }
+
+    )
+    
+  } catch (error) {
+    
+  }
+}
 
 export default function App() {
+
+  const [counter, setCounter] = useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View 
+    style={styles.container}>
+      <Pressable
+        onPress={()=> setCounter(counter + 1)}
+        >
+        <Text>Clicks</Text>
+
+      </Pressable>
+      <Text>{counter}</Text>
+      <Button 
+        onPress={askForPhoneCallPermission}
+        title="Permisos"/>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
